9c6c0fcd81b4aeec7419c775b36b7f502f26f811
Ufuk Emre Yücetürk <ufukemreyuceturk@hotmail.com>
2023-11-17 21:32:30 +0300

    models and migration

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(max_length=6)
+    bookingdate = models.DateTimeField()
+
+class Booking(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
